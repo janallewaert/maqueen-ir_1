00726813c7ffcf453cdfdc197bdc85ae1ed1eeaa
ALLEWAERT JAN <64103952+janallewaert@users.noreply.github.com>
2023-01-12 11:52:44 +0100

    IR Maqueen forward and stop

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 IR.IR_callbackUser(function (message) {
     basic.showNumber(message)
+    if (message == 5) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    } else if (message == 77) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+    	
+    }
 })
-basic.showIcon(IconNames.Confused)
+basic.showIcon(IconNames.No)
